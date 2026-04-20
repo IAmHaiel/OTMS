62ca0eb40fdeccebe27918820c68425764c52358
IAmHaiel <mikhjnelo@gmail.com>
2026-04-20 19:20:17 +0800

    Added AuthController

---
+++ OTMS/Controllers/AuthController.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using OTMS.Entities.DTOs;
+using OTMS.Entities.Models;
+using OTMS.Service.Interfaces;
+
+namespace OTMS.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController(IAuthService authService) : ControllerBase
+    {
+
+        // Authentication APIs
+        [HttpPost("register")]
+        public async Task<ActionResult<Employee>> Register(EmployeeRegisterDTO request)
+        {
+            var user = await authService.RegisterAsync(request);
+            if (user is null)
+            {
+                return BadRequest("Employee Number already exists.");
+            }
+
+            return Ok(user);
+        }
+
+        [HttpPost("login")]
+        public async Task<ActionResult<TokenResponseDTO>> Login(EmployeeLoginDTO request)
+        {
+            var result = await authService.LoginAsync(request);
+
+            if (result is null)
+            {
+                return BadRequest("Invalid Employee Number or password");
+            }
+
+            return Ok(result);
+        }
+
+        [HttpPost("refresh-token")]
+        public async Task<ActionResult<TokenResponseDTO>> RefreshToken(RefreshTokenRequestDTO request)
+        {
+            var result = await authService.RefreshTokensAsync(request);
+            if (result is null
+                || result.AccessToken is null
+                || result.RefreshToken is null)
+            {
+                return Unauthorized("Invalid refresh token.");
+            }
+
+            return Ok(result);
+        }
+
+
+        // Secured APIs
+        [Authorize]
+        [HttpGet]
+        public IActionResult AuthenticatedOnlyEndpoint()
+        {
+            return Ok("You are authenticated");
+        }
+
+    }
+}
