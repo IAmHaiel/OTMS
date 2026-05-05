8c2292b969e22a52d49a9f35f58c8dea1f377cdf
IAmHaiel <mikhjnelo@gmail.com>
2026-05-05 11:36:00 +0800

    Added TaskComment.cs in Models

---
+++ OTMS/Entities/Models/TaskComment.cs
@@ -0,0 +1,16 @@
+﻿namespace OTMS.Entities.Models
+{
+    public class TaskComment
+    {
+        public Guid CommentId { get; set; }
+        public Guid EmployeeId { get; set; }
+        public Guid TaskId { get; set; }
+
+        public string Message { get; set; } = string.Empty;
+
+        public DateTime CreatedAt { get; set; }
+        public DateTime? UpdatedAt { get; set; }
+
+        // Navigation properties
+    }
+}
