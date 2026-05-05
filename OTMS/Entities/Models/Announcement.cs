e5148f52ff1d82112d4f6ccb79c38cc1500900a4
IAmHaiel <mikhjnelo@gmail.com>
2026-05-05 11:38:05 +0800

    Added in Announcement.cs in Models

---
+++ OTMS/Entities/Models/Announcement.cs
@@ -0,0 +1,17 @@
+﻿namespace OTMS.Entities.Models
+{
+    public class Announcement
+    {
+        public Guid AnnouncementId { get; set; }
+        public Guid CreatedBy { get; set; }
+
+        public string Title { get; set; } = string.Empty;
+        public string Content { get; set; } = string.Empty;
+        public string TargetRole { get; set; } = string.Empty;
+
+        public DateTime CreatedAt { get; set; }
+        public DateTime? UpdatedAt { get; set; }
+
+        // Navigation properties
+    }
+}
