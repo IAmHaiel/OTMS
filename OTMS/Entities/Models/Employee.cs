e86f71301ee34d14da132d31815745b534aec60a
IAmHaiel <mikhjnelo@gmail.com>
2026-05-05 11:28:04 +0800

    Added few variables in Employee.cs

--- OTMS/Entities/Models/Employee.cs
+++ OTMS/Entities/Models/Employee.cs
@@ -2,13 +2,20 @@
 {
     public class Employee
     {
-        public Guid Id { get; set; }
+        public Guid EmployeeId { get; set; }
         public string EmployeeNumber { get; set; } = string.Empty;
         public string EmployeeName { get; set; } = string.Empty;
         public string ContactNumber { get; set; } = string.Empty;
+
+        public DateTime CreatedAt { get; set; }
+        public DateTime? UpdatedAt { get; set; }
+
         public string PasswordHash { get; set; } = string.Empty;
         public string Role { get; set; } = string.Empty;
         public string? RefreshToken { get; set; }
         public DateTime? RefreshTokenExpiryTime { get; set; }
+
+        // Navigation properties
+
     }
 }
