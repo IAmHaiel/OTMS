b13aefbf195faa697a8a8b9facdab45bf01d7d88
IAmHaiel <mikhjnelo@gmail.com>
2026-05-05 11:34:11 +0800

    Added Task.cs in Models

---
+++ OTMS/Entities/Models/Task.cs
@@ -0,0 +1,24 @@
+﻿namespace OTMS.Entities.Models
+{
+    public class Task
+    {
+
+        public Guid TaskId { get; set; }
+
+        public Guid CreatedBy { get; set; }
+        public Guid AssignedTo { get; set; }
+        public Guid? EvaluatedBy { get; set; }
+
+        public string TaskTitle { get; set; } = string.Empty;
+        public string? TaskDescription { get; set; }
+        public string Priority { get; set; } = "Normal";
+        public DateTime? DueAt { get; set; }
+
+        public string? TaskRemarks { get; set; }
+        public string TaskStatus { get; set; } = "Pending";
+
+        public DateTime CreatedAt { get; set; }
+        public DateTime? UpdatedAt { get; set; }
+
+    }
+}
