19fd69942ba49f21284a47b5ddfaf610992c92d0
IAmHaiel <mikhjnelo@gmail.com>
2026-05-05 11:41:46 +0800

    Added ActivityLog.cs in Models

---
+++ OTMS/Entities/Models/ActivityLog.cs
@@ -0,0 +1,15 @@
+﻿namespace OTMS.Entities.Models
+{
+    public class ActivityLog
+    {
+        public Guid LogId { get; set; }
+        public Guid AccountId { get; set; }
+
+        public string ActivityType { get; set; } = string.Empty;
+        public string? Description { get; set; } = string.Empty;
+
+        public DateTime CreatedAt { get; set; }
+
+        // Navigation properties
+    }
+}
