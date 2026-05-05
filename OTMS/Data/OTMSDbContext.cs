4db92457fa28343306b0ad974400c6905dfb4f62
IAmHaiel <mikhjnelo@gmail.com>
2026-05-05 12:03:21 +0800

    Added DbSets in OTMSDbContext.cs

--- OTMS/Data/OTMSDbContext.cs
+++ OTMS/Data/OTMSDbContext.cs
@@ -7,5 +7,11 @@ namespace OTMS.Data
     public class OTMSDbContext(DbContextOptions<OTMSDbContext> options) : DbContext(options)
     {
         public DbSet<Employee> Employees { get; set; }
+        public DbSet<Account> Accounts { get; set; }
+        public DbSet<Entities.Models.Task> Tasks { get; set; }
+        public DbSet<TaskComment> TaskComments { get; set; }
+        public DbSet<Announcement> Announcements { get; set; }
+        public DbSet<Notification> Notifications { get; set; }
+        public DbSet<ActivityLog> ActivityLogs { get; set; }
     }
 }
