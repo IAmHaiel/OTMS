f3c7fc342e86b1fd347351aad9f45d907de766fb
IAmHaiel <mikhjnelo@gmail.com>
2026-05-05 11:40:09 +0800

    Added Notification.cs in Models

---
+++ OTMS/Entities/Models/Notification.cs
@@ -0,0 +1,20 @@
+﻿namespace OTMS.Entities.Models
+{
+    public class Notification
+    {
+        public Guid NotificationId { get; set; }
+        public Guid EmployeeId { get; set; }
+
+        public Guid? TaskId { get; set; }
+        public Guid? AnnouncementId { get; set; }
+
+        public string NotificationType { get; set; } = string.Empty;
+        public string Message { get; set; } = string.Empty;
+
+        public bool IsRead { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+
+        // Navigation properties
+    }
+}
