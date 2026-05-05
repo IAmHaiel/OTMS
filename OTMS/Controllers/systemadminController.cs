db3bcda25c8c8b8cec4cc72f2d62f54da05bf19a
IAmHaiel <mikhjnelo@gmail.com>
2026-05-05 11:23:39 +0800

    Added systemadmin Controller

---
+++ OTMS/Controllers/systemadminController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using OTMS.Service.Interfaces;
+
+namespace OTMS.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class systemadminController(IAuthService authService) : ControllerBase
+    {
+
+    }
+}
