13c36a14d77068842b52c7d38cef13ed3bb6076c
IAmHaiel <mikhjnelo@gmail.com>
2026-05-08 11:57:38 +0800

    Added DTO for Assigning Roles

---
+++ OTMS/Entities/DTOs/AccountManagement/AssignUserRoleDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace OTMS.Entities.DTOs.AccountManagement
+{
+    public class AssignUserRoleDTO
+    {
+        public string EmployeeNumber { get; set; } = string.Empty;
+        public string RoleName { get; set; } = string.Empty;
+    }
+}
---
+++ OTMS/Entities/DTOs/AccountManagement/Responses/AssignUserRoleResponseDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace OTMS.Entities.DTOs.AccountManagement.Responses
+{
+    public class AssignUserRoleResponseDTO
+    {
+        public string EmployeeNumber { get; set; } = string.Empty;
+        public string RoleName { get; set; } = string.Empty;
+        public bool Success { get; set; }
+        public DateTime AssignedAt { get; set; }
+    }
+}
