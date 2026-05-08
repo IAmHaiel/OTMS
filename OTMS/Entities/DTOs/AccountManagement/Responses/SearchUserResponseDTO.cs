2d4cfd14e71e9d6699338b17754cb6df5a56fc7b
IAmHaiel <mikhjnelo@gmail.com>
2026-05-08 12:47:22 +0800

    Added DTOs for Searching Users

---
+++ OTMS/Entities/DTOs/AccountManagement/Responses/SearchUserResponseDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace OTMS.Entities.DTOs.AccountManagement.Responses
+{
+    public class SearchUserResponseDTO
+    {
+        public string EmployeeNumber { get; set; } = string.Empty;
+        public string EmployeeName { get; set; } = string.Empty;
+        public string Role { get; set; } = string.Empty;
+        public string AccountStatus { get; set; } = string.Empty;
+        public bool Success { get; set; }
+    }
+}
---
+++ OTMS/Entities/DTOs/AccountManagement/SearchUserDTO.cs
@@ -0,0 +1,12 @@
+﻿namespace OTMS.Entities.DTOs.AccountManagement
+{
+    public class SearchUserDTO
+    {
+        // The system shall allow Systems Admin to search employee accounts by employee Number, name, or role.
+
+        public string? EmployeeNumber { get; set; }
+        public string? EmployeeName { get; set; }
+        public string? Role { get; set; }
+
+    }
+}
