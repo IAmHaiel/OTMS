b25b20916ac96f3c9fd605b1b37d3804006ade2d
IAmHaiel <mikhjnelo@gmail.com>
2026-05-08 15:00:58 +0800

    Implemented View Profile

---
+++ OTMS/Controllers/profileController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using OTMS.Service.Interfaces;
+
+namespace OTMS.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class profileController(IProfileService profileService) : ControllerBase
+    {
+        /// <summary>
+        /// View Profile from the System. Only accessible to users that are within the scoped role and authenticated.
+        /// </summary>
+        [Authorize(Roles = "SystemAdmin,OperationAdmin,Coordinator,Encoder")]
+        [HttpGet("view-profile")]
+        public async Task<IActionResult> ViewProfile()
+        {
+            var result = await profileService.ViewProfile();
+            if(result is null)
+            {
+                return NotFound(new { Message = "Employee not found." });
+            }
+            return Ok(result);
+        }
+        
+
+
+
+    }
+}
--- OTMS/Service/Services/ProfileService.cs
+++ OTMS/Service/Services/ProfileService.cs
@@ -1,12 +1,15 @@
-﻿using OTMS.Data;
+﻿using Microsoft.EntityFrameworkCore;
+using OTMS.Data;
 using OTMS.Entities.DTOs.Profile;
 using OTMS.Entities.DTOs.Profile.Responses;
 using OTMS.Service.Interfaces;
+using System.Security.Claims;
 
 namespace OTMS.Service.Services
 {
     public class ProfileService(
-        OTMSDbContext context
+        OTMSDbContext context,
+        IHttpContextAccessor httpContextAccessor
         ) : IProfileService
     {
         public Task<ChangePasswordResponseDTO?> ChangePassword(ChangePasswordDTO request)
@@ -19,9 +22,35 @@ namespace OTMS.Service.Services
             throw new NotImplementedException();
         }
 
-        public Task<ViewProfileResponseDTO?> ViewProfile()
+
+        public async Task<ViewProfileResponseDTO?> ViewProfile()
         {
-            throw new NotImplementedException();
+            var claimProfile = httpContextAccessor
+                .HttpContext?
+                .User
+                .FindFirst(ClaimTypes.NameIdentifier)?
+                .Value;
+
+            if (string.IsNullOrEmpty(claimProfile))
+                return null;
+
+            var profile = await context.Employees
+                .Include(e => e.Account)
+                .FirstOrDefaultAsync(e => e.Account.AccountId.ToString() == claimProfile);
+
+            if (profile is null && profile.Account is null)
+                return null;
+
+            return new ViewProfileResponseDTO
+            {
+                EmployeeNumber = profile.EmployeeNumber,
+                EmployeeName = profile.EmployeeName,
+                ContactNumber = profile.ContactNumber,
+                AccountStatus = profile.Account.AccountStatus,
+                Role = profile.Account.Role,
+                CreatedAt = profile.CreatedAt,
+                UpdatedAt = profile.UpdatedAt ?? profile.CreatedAt
+            };
         }
     }
 }
