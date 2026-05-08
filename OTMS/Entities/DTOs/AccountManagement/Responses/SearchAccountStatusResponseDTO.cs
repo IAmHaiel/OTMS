454ed20b0531e22c252ef8115d33092de481e057
IAmHaiel <mikhjnelo@gmail.com>
2026-05-08 15:55:24 +0800

    Implemented Search User by Account Status

--- OTMS/Entities/DTOs/AccountManagement/Responses/SearchAccountStatusResponseDTO.cs
+++ OTMS/Entities/DTOs/AccountManagement/Responses/SearchAccountStatusResponseDTO.cs
@@ -4,6 +4,7 @@
     {
         public string EmployeeNumber { get; set; } = string.Empty;
         public string EmployeeName { get; set; } = string.Empty;
+        public string ContactNumber { get; set; } = string.Empty;
         public string Role { get; set; } = string.Empty;
         public string AccountStatus { get; set; } = string.Empty;
         public bool Success { get; set; }
